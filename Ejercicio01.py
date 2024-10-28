aef12e6696414eabb34bf19e942698906ad3c18d
sgimenelan <sgimenelan@educacion.navarra.es>
2024-10-28 11:11:19 +0100

    Update Ejercicio01.py

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -1,8 +1,10 @@
 #Escribir un programa que almacene las asignaturas de un curso 
 #(por ejemplo Matemáticas, Física, Química, Historia y Lengua)
 #en una lista y la muestre por pantalla.
-numero = int(input("Dime el numero de asignaturas que tienes: "))
 lista = []
-for i in range(numero):
-    lista.append(input("Dime la asignatura: "))
-print(lista)
+asignatura = ""
+while asignatura != "no tengo mas":
+    asignatura = input("Dime la asignatura: ")
+    lista.append(asignatura)
+numero = (len(lista) - 1)
+print(lista[0:numero])
