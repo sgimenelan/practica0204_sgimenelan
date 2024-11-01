4d7b569bf3456f3fb21d631a2450bc368b717d05
sgimenelan <sgimenelan@educacion.navarra.es>
2024-11-01 12:12:28 +0100

    Create Ejercicio12.py

---
+++ Ejercicio12.py
@@ -0,0 +1,10 @@
+#Escribir un programa que pregunte por una muestra de
+#  números, separados por comas, los guarde en una lista
+#  y muestre por pantalla su media y desviación típica.
+import statistics
+numeros = list(input("Dime unos numeros separados por comas: "))
+while "," in numeros:
+    numeros.remove(",")
+numeros = list(map(float, numeros))
+print("La medias es: ", statistics.mean(numeros))
+print("La medias es: ", statistics.stdev(numeros))
